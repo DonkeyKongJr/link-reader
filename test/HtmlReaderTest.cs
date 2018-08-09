2ec54d245dc001eda8523dd00677ebece0607b5e
Patrick Schadler <pschadler@pmone.com>
2018-08-09 13:05:34 +0200

    feat(xunit): add test project and basic test

---
+++ test/HtmlReaderTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LinkReaderTest
+{
+    public class HtmlReaderTest
+    {
+        [Fact]
+        public void ShouldTestXUnitTestRunnable()
+        {
+             Assert.Equal(4, 2+2);
+        }
+    }
+}
