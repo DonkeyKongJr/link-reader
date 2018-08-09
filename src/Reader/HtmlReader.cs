ade8cab3c59e7c2407eb040151f7d026b6a5c48d
Patrick Schadler <patrick.schadler@gmail.com>
2018-08-09 08:23:27 +0200

    feature(html-reader): add get request to get html content of url

--- src/Reader/HtmlReader.cs
+++ src/Reader/HtmlReader.cs
@@ -1,9 +1,40 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 
 public class HtmlReader : IReader
 {
-    public IEnumerable<string> getLinksFromUrl(string url)
+    public IEnumerable<string> GetLinksFromUrl(string url)
     {
         throw new System.NotImplementedException();
+    }
+
+    private string GetHtmlCodeFromUrl(string url)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader readStream = null;
+
+            if (response.CharacterSet == null)
+            {
+                readStream = new StreamReader(receiveStream);
+            }
+            else
+            {
+                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+            }
+            string data = readStream.ReadToEnd();
+
+            response.Close();
+            readStream.Close();
+
+            return data;
+        }
+        return string.Empty;
     }
 }
