4476173611be75640d67e8b8bd64099eb682898f
Patrick Schadler <patrick.schadler@gmail.com>
2018-08-11 09:59:40 +0200

    feat(ci)

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd src
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe begin /n:link-reader /k:link-reader /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.opencover.reportsPaths="./test/code-coverage/coverage.opencover.xml"
