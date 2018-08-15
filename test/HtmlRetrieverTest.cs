1f0626b1c8570b065821b7810224b618fd21d39c
Patrick Schadler <patrick.schadler@gmail.com>
2018-08-15 11:56:50 +0200

    feat(moq): add unit test with mocks for web requests/response

--- test/HtmlRetrieverTest.cs
+++ test/HtmlRetrieverTest.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Linq;
+using System.Net;
 using LinkReader.Installer;
+using LinkReader.Reader;
 using LinkReader.Retriever;
+using LinkReader.Retriever.Interfaces;
+using Moq;
 using Xunit;
 
 namespace LinkReaderTest
@@ -30,5 +34,22 @@ namespace LinkReaderTest
             Assert.Throws<NotSupportedException>(reader);
         }
 
+        [Fact]
+        public void ShouldReturnEmptyStringWhenStatusCodeIsNotOk()
+        {
+            var moq = new Mock<IWebRequest>();
+            var moqHttpWebRequest = new Mock<HttpWebRequest>();
+            var moqHttpWebResponse = new Mock<HttpWebResponse>();
+
+            moqHttpWebResponse.Setup(_ => _.StatusCode).Returns(HttpStatusCode.BadRequest);
+            moqHttpWebRequest.Setup(_ => _.GetResponse()).Returns(moqHttpWebResponse.Object);
+            moq.Setup(_ => _.Create(It.IsAny<string>())).Returns(moqHttpWebRequest.Object);
+
+            var retriever = new HtmlRetriever(moq.Object);
+
+            var result = retriever.Retrieve("test");
+
+            Assert.Empty(result);
+        }
     }
 }
