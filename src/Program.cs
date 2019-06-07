37783be58057081310808a397ccb1308f45bdfa8
Patrick Schadler <patrick.schadler@gmail.com>
2019-06-07 11:21:22 +0200

    add sentry

--- src/Program.cs
+++ src/Program.cs
@@ -8,6 +8,7 @@ using LinkReader.Reader;
 using LinkReader.Retriever;
 using LinkReader.Validator;
 using Microsoft.Extensions.DependencyInjection;
+using Sentry;
 
 namespace LinkReader
 {
@@ -16,41 +17,44 @@ namespace LinkReader
     {
         static void Main(string[] args)
         {
-            var provider = ServiceInstaller.Install();
-            var urlProtocolHandler = provider.GetService<IHandler<string, string>>();
-            var urlValidator = provider.GetService<IValidator>();
-            var linkReaderManager = provider.GetService<LinkReaderManager>();
-
-            while (true)
+            using (SentrySdk.Init("https://97b4c7473d8c4307a63bd065b14512f7@sentry.io/1477075"))
             {
-                Console.WriteLine("Welcome to link reader!");
-                Console.WriteLine("I will show you every link which is present on a html site");
-                Console.WriteLine("Please provide a webiste and press [ENTER]");
+                var provider = ServiceInstaller.Install();
+                var urlProtocolHandler = provider.GetService<IHandler<string, string>>();
+                var urlValidator = provider.GetService<IValidator>();
+                var linkReaderManager = provider.GetService<LinkReaderManager>();
 
-                var url = Console.ReadLine();
-
-                Console.WriteLine("Please provide the depth of the crawler [ENTER]");
-                var depthText = Console.ReadLine();
-
-                uint depth;
-
-                if (!uint.TryParse(depthText, out depth))
+                while (true)
                 {
-                    Console.WriteLine("Entered depth was not valid. Please start again.");
-                    continue;
-                }
+                    Console.WriteLine("Welcome to link reader!");
+                    Console.WriteLine("I will show you every link which is present on a html site");
+                    Console.WriteLine("Please provide a webiste and press [ENTER]");
 
-                try
-                {
-                    var links = linkReaderManager.Run(url, depth, provider);
-                    links.ToList().ForEach(Console.WriteLine);
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                    var url = Console.ReadLine();
 
-                Console.ReadKey();
+                    Console.WriteLine("Please provide the depth of the crawler [ENTER]");
+                    var depthText = Console.ReadLine();
+
+                    uint depth;
+
+                    if (!uint.TryParse(depthText, out depth))
+                    {
+                        Console.WriteLine("Entered depth was not valid. Please start again.");
+                        continue;
+                    }
+
+                    try
+                    {
+                        var links = linkReaderManager.Run(url, depth, provider);
+                        links.ToList().ForEach(Console.WriteLine);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    Console.ReadKey();
+                }
             }
         }
     }
